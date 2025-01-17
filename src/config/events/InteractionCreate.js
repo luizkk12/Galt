19d8684e7279e06f3e2fb1284f9acb65983ada38
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-01-16 23:04:25 -0300

    Update InteractionCreate.js

--- src/config/events/InteractionCreate.js
+++ src/config/events/InteractionCreate.js
@@ -25,10 +25,10 @@ module.exports = {
       } catch (error) {
         console.error(error);
         if (interaction.replied || interaction.deferred) {
-			       await interaction.followUp({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
-		      } else {
-			       await interaction.reply({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
-		      }
+	  await interaction.followUp({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
+	} else {
+	  await interaction.reply({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
+        }
       }
     }
   }
