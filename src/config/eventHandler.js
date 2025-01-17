e73697b323402389584e81959daff8cbe45bcd9f
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-01-16 22:51:15 -0300

    Add files via upload

--- src/config/eventHandler.js
+++ src/config/eventHandler.js
@@ -1,1 +1,18 @@
+const { readdirSync } = require('node:fs');
+const { join } = require('node:path');
 
+module.exports = async (client) => {
+  const eventsPath = join(__dirname, 'events');
+  const eventsFile = readdirSync(eventsPath).filter(e => e.endsWith('.js'));
+  
+  for (const file of eventsFile) {
+    const files = join(eventsPath, file);
+    const event = require(files);
+    
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
+}
