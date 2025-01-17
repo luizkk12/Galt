7b717f9d899d767b39bcf3f805cecff0173891ba
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-01-16 22:52:48 -0300

    Add files via upload

--- src/config/events/ClientReady.js
+++ src/config/events/ClientReady.js
@@ -1,1 +1,24 @@
+const { Events, ChannelType, ActivityType } = require('discord.js');
 
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute: (readyClient) => {
+    console.log(`[BOT ONLINE] ${readyClient.user.tag} está online.`);
+
+    let atividades = [
+      `💻 Sou programado em discord.js, na versão 14!`,
+      `😎 Estou em ${readyClient.guilds.cache.size} servidores!`,
+       `👀 Estou ajudando ${readyClient.users.cache.filter(u => !u.bot).size} usuários!`,
+       `⚙️ Tenho ${readyClient.slashCommands.size} comandos!`,
+       `🤩 Estou presente em ${readyClient.channels.cache.filter(c => c.type === ChannelType.GuildText).size} canais!`
+    ];
+
+    let index = 0;
+
+    setInterval(() => {
+      readyClient.user.setActivity(atividades[index], { type: ActivityType.Playing });
+      index = (index + 1) % 5;
+    }, 15_000);
+  }
+}
---
+++ src/config/events/InteractionCreate.js
@@ -0,0 +1,35 @@
+const { Events, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+  name: Events.InteractionCreate,
+  execute: async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+
+      const clientCommand = interaction.client.slashCommands.get(interaction.commandName);
+
+      if (!clientCommand) {
+        console.log(`🤔 Nenhum comando chamado "${interaction.commandName}" foi encontrado.`);
+        return;
+      }
+
+      if (interaction.guild) {
+        let botPerm = interaction.channel.permissionsFor(interaction.client.user.id);
+
+        if (!botPerm || !botPerm.has(PermissionsBitField.Flags.SendMessages)) {
+          console.log(`❌ Não posso enviar mensagens em: ${interaction.channel}.`);
+        }
+      }
+
+      try {
+        clientCommand.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+			       await interaction.followUp({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
+		      } else {
+			       await interaction.reply({ content: 'Houve um erro ao executar!', flags: MessageFlags.Ephemeral });
+		      }
+      }
+    }
+  }
+}
